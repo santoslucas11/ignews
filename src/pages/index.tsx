392fd95810138982f13740952896a8cfab97bfc1
Lucas Rocha <santoslucas.dev@gmail.com>
2021-12-30 21:22:50 -0300

    Título dinâmico por página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Teste</h1>
+    <>
+      <Head>
+        <title>
+          Inicio | ig.news
+        </title>
+      </Head>
+      <h1 className={styles.title}>
+        Teste
+      </h1>
+    </>
   )
 }
